77632adf34caee7c413835d2ad35d9c5372f5025
Ismael <ismaelberfer@gmail.com>
2016-03-06 14:36:49 +0100

    primer commit con el modelo de post creado

---
+++ web/controllers/post_controller.ex
@@ -0,0 +1,64 @@
+defmodule Blog.PostController do
+  use Blog.Web, :controller
+
+  alias Blog.Post
+
+  plug :scrub_params, "post" when action in [:create, :update]
+
+  def index(conn, _params) do
+    render(conn, "index.html", posts: Repo.all(Post))
+  end
+
+  def new(conn, _params) do
+    render(conn, "new.html", changeset: Post.changeset(%Post{}))
+  end
+
+  def create(conn, %{"post" => post_params}) do
+    changeset = Post.changeset(%Post{}, post_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _post} ->
+        conn
+        |> put_flash(:info, "Post created successfully.")
+        |> redirect(to: post_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    render(conn, "show.html", post: Repo.get!(Post, id))
+  end
+
+  def edit(conn, %{"id" => id}) do
+    post = Repo.get!(Post, id)
+    changeset = Post.changeset(post)
+    render(conn, "edit.html", post: post, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    post = Repo.get!(Post, id)
+    changeset = Post.changeset(post, post_params)
+
+    case Repo.update(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post updated successfully.")
+        |> redirect(to: post_path(conn, :show, post))
+      {:error, changeset} ->
+        render(conn, "edit.html", post: post, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    post = Repo.get!(Post, id)
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(post)
+
+    conn
+    |> put_flash(:info, "Post deleted successfully.")
+    |> redirect(to: post_path(conn, :index))
+  end
+end
